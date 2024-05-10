114838025d54fa74ae1f2c5eb6530a82f5686a0a
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-10 12:11:44 +0300

    products page

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Product, Attribute, Value
 
+
 class AttributeAdmin(admin.ModelAdmin):
     exclude = ['name_for_admin_page']
+
 
 class ValueAdmin(admin.ModelAdmin):
     exclude = ['name_for_admin_page']
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from categories.models import Category
-from store.models import Product
+from store.models import Product, Value, Attribute
 
 
 def index(request):
@@ -11,7 +11,17 @@ def index(request):
 def category_products(request, category_name):
     category = Category.objects.get(name=category_name)
     products = Product.objects.filter(category=category)
-    return render(request, 'store/index.html', {'products': products})
+    attributes = Attribute.objects.filter(is_show=True)
+
+    attributes_dict = {}
+    for attribute in attributes:
+        values = Value.objects.filter(attribute=attribute).values('value').distinct()
+        attributes_dict[attribute] = values
+
+    min_price = min(products.values_list('price', flat=True))
+    max_price = max(products.values_list('price', flat=True))
+
+    return render(request, 'store/index.html', {'min_price': min_price, 'max_price': max_price, 'products': products, 'attributes_dict': attributes_dict})
 
 
 def product_page(request, product_name):
