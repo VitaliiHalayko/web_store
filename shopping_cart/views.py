6a2c2c541a8d0128645f8dccb465a747ef283e48
Marko Kostiuk <marko_kostiuk@knu.ua>
2024-05-22 21:00:43 +0300

    creating order is done

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from django.conf import settings
 from store.models import Product
-from .models import CartItem
+from .models import CartItem, Order, OrderItem
 
 
 def add_to_cart(request, product_id, quantity):
@@ -56,8 +56,41 @@ def change_quantity(request, product_id, quantity):
         'totalSumWithSales': sum(items)[1]
     })
 
-def create_order(request):
-    pass
+
+def create_order(request, lastName, firstName, email, city, address, postal):
+    session_key = request.session.session_key
+    items = CartItem.objects.filter(session_key=session_key)
+
+    order = Order.objects.create(
+        last_name=lastName,
+        first_name=firstName,
+        email=email,
+        city=city,
+        address=address,
+        postal_code=postal,
+        amount=sum(items)[1]
+    )
+
+    for item in items:
+        OrderItem.objects.create(
+            product=item.product,
+            order=order,
+            quantity=item.quantity
+        )
+
+    CartItem.objects.filter(session_key=session_key).delete()
+    items = CartItem.objects.filter(session_key=session_key)
+
+    messages.success(request, f'Замовлення створене! Очікуйте повідомлення на вказану пошту.')
+    return render(request, 'shopping_cart/cart.html', context={
+        'items': items,
+        'count': len(items),
+        'totalSumWithoutSales': sum(items)[0],
+        'totalSumWithSales': sum(items)[1]
+    })
+
+
+
 
 def sum(items):
     totalSumWithoutSales = 0
