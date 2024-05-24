4e92b297bafd7ba11716eb7df101277cb5c8a04c
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-24 20:14:37 +0300

    fix bugs

--- web_store/settings.py
+++ web_store/settings.py
@@ -31,9 +31,9 @@ DEBUG = False
 # Email sending settings
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
